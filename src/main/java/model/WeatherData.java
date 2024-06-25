cea78fb99af04d0be30c751c895ab8c075a7b5c5
Lucas Assis Pereira <lucasassis1905@gmail.com>
2024-06-25 16:29:56 -0300

    Criada a Main e o model da WeatherData

--- src/main/java/model/WeatherData.java
+++ src/main/java/model/WeatherData.java
@@ -1,4 +1,15 @@
 package model;
 
+import java.util.List;
+
 public class WeatherData {
+    private List<Double> temperature_2m;
+
+    public List<Double> getTemperature2m() {
+        return temperature_2m;
+    }
+
+    public void setTemperature2m(List<Double> temperature_2m) {
+        this.temperature_2m = temperature_2m;
+    }
 }
